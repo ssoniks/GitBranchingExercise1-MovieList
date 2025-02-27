3cf33b8e9e1793d92ba681ab94e173e24804d494
pishlo <soni.kozaliev@gmail.com>
2025-02-27 16:39:51 +0100

    feat(initial-setup): added models/movie.js movie model and /movie route to test movie creation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
+const Movie = require('./models/movie')
 
 app.set('view engine', 'ejs');
 
@@ -15,4 +16,19 @@ app.listen(3000);
 app.get('/', (req, res) => {
 
     res.render("index");
+});
+
+app.get('/movie', (req, res) => {
+
+    const movie = new Movie({
+        title: "Inception",
+        genre: "Sci-Fi",
+        releaseYear: 2010,
+        director: "Christopher Nolan",
+        rating: 8.8
+    });
+
+    movie.save()
+        .then((result) => console.log(result))
+        .catch((err) => console.log(err));
 });
